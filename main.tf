6dc74477c152f709f593f288f7a75de476af60d0
wildlifehexagon <crossthebreeze@gmail.com>
2019-07-17 16:53:20 -0500

    feat: add graphql server and frontend web apps

--- main.tf
+++ main.tf
@@ -259,17 +259,84 @@ resource "helm_release" "stock-api-server" {
   ]
 }
 
-## need modware-annotation
+## modware-annotation is not officially indexed yet
+# resource "helm_release" "annotation-api-server" {
+#   name = "annotation-api-server"
+#   chart = "dictybase/annotation-api-server"
+#   namespace = "dictybase"
 
-## need authserver
+#   values = [
+#     "${var.config_path}/annotation-api-server/${var.env}.yaml"
+#   ]
+# }
 
-## then graphql server
+##
+## need authserver here
+## authserver requires values set from the command line
+## publicKey, privateKey, configFile
+##
+
+## -- graphql server
+resource "helm_release" "graphql-server" {
+  name = "graphql-server"
+  chart = "dictybase/graphql-server"
+  namespace = "dictybase"
+}
 
 ## data loaders go here
 
 ## install kubeless functions here
 
-## install frontend web apps here
+## frontend web apps
+resource "helm_release" "dicty-stock-center" {
+  name = "dicty-stock-center"
+  chart = "dictybase/dicty-stock-center"
+  namespace = "dictybase"
+
+  values = [
+    "${var.config_path}/dicty-stock-center/${var.env}.yaml"
+  ]
+}
+
+resource "helm_release" "dicty-frontpage" {
+  name = "dicty-frontpage"
+  chart = "dictybase/dicty-frontpage"
+  namespace = "dictybase"
+
+  values = [
+    "${var.config_path}/dicty-frontpage/${var.env}.yaml"
+  ]
+}
+
+resource "helm_release" "genomepage" {
+  name = "genomepage"
+  chart = "dictybase/genomepage"
+  namespace = "dictybase"
+
+  values = [
+    "${var.config_path}/genomepage/${var.env}.yaml"
+  ]
+}
+
+resource "helm_release" "dictyaccess" {
+  name = "dictyaccess"
+  chart = "dictybase/dictyaccess"
+  namespace = "dictybase"
+
+  values = [
+    "${var.config_path}/dictyaccess/${var.env}.yaml"
+  ]
+}
+
+resource "helm_release" "publication" {
+  name = "publication"
+  chart = "dictybase/publication"
+  namespace = "dictybase"
+
+  values = [
+    "${var.config_path}/publication/${var.env}.yaml"
+  ]
+}
 
 ## -- create config folder tree and stub values files
 resource "null_resource" "tree_value_files" {
