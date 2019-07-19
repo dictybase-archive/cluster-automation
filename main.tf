5fa19d619e42d4d218cc60b9aba371af4444ea3e
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2019-07-19 13:03:36 -0500

    refactor: add provider and data blocks in main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,42 @@ terraform {
   required_version = ">= 0.12"
 }
 
+provider "kubernetes" {}
+provider "helm" {
+    kubernetes {}
+}
+
+provider "github" {
+  token = local.github_token_data
+  organization = "${var.github_organization}"
+}
+
+data "helm_repository" "stable" {
+  name = "stable"
+  url = "http://storage.googleapis.com/kubernetes-charts"
+}
+
+
+data "helm_repository" "dictybase" {
+  name = "dictybase"
+  url = "https://dictybase-docker.github.io/kubernetes-charts"
+}
+
+data "helm_repository" "incubator" {
+  name = "incubator"
+  url = "https://kubernetes-charts-incubator.storage.googleapis.com"
+}
+
+data "helm_repository" "argo" {
+  name = "argo"
+  url = "https://argoproj.github.io/argo-helm"
+}
+
+data "helm_repository" "jetstack" {
+  name = "jetstack"
+  url = "https://charts.jetstack.io"
+}
+
 ## -- cluster admin
 resource "kubernetes_cluster_role_binding" "cluster_admin" {
   metadata {
