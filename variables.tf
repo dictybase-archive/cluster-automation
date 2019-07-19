7d8cb5bfff993f42b3de8e288d95e69f0298df0b
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2019-07-19 14:49:06 -0500

    refactor: remove argo module specific variables from root file

--- variables.tf
+++ variables.tf
@@ -55,14 +55,6 @@ variable "dictybase_arango_create_db_version" {
   default = "0.0.3"
 }
 
-variable "argo_events_version" {
-  default = "0.4.2"
-}
-
-variable "argo_workflow_version" {
-  default = "0.4.0"
-}
-
 variable  "arangodb_storage_size" {
   default = "50Gi"
 }
@@ -83,60 +75,4 @@ variable "env" {}
 
 variable "env_value_files"  {
   default = ["dev.yaml", "staging.yaml", "prod.yaml"]
-}
-
-variable "argo_namespace" {
-  default = "argo"
-}
-
-variable "argo_service_account" {
-  default = "argo-workflow"
-}
-
-variable "minio_secret" {
-  default = "minio-secret"
-}
-
-
-variable "docker_secret" {
-  default = "docker-secret"
-}
-
-variable "docker_config_path" {
-  default = "~/.docker/config.json"
-}
-
-variable "slack_secret" {
-  default = "slack-secret"
-}
-
-variable "github_secret" {
-  default = "github-access"
-}
-
-variable "slack_secret_data" {}
-
-variable "github_token_path" {
-  default = "~/.github-webhook"
-}
-
-variable "github_organization" {
-  default = "dictyBase"
-}
-
-variable "github_repositories" {
-  default = [
-    "dicty-stock-center",
-    "dicty-frontpage",
-    "genomepage",
-    "dictyaccess",
-    "publication",
-    "modware-stock",
-    "modware-order",
-    "modware-annotation",
-    "modware-identity",
-    "modware-user",
-    "modware-content",
-    "graphql-server",
-  ]
 }
