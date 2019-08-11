db06a88d6b1c9b29ab31da6399e4b00ef849a9f4
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2019-08-11 08:48:01 -0500

    feat: add install of issuer-certificate chart

--- modules/keel/main.tf
+++ modules/keel/main.tf
@@ -3,6 +3,14 @@ data "helm_repository" "keel" {
   url = "https://charts.keel.sh"
 }
 
+resource "helm_release" "keel-issuer-certificate" {
+  name      = "keel-issuer-certificate"
+  chart     = "dictybase/issuer-certificate"
+  namespace = "kube-system"
+  version   = "${var.issuer_certificate_version}"
+  values    = ["${var.config_path}/keel/issuer-cert-${var.env}.yaml"]
+}
+
 resource "helm_release" "keel" {
   name = "keel"
   chart = "keel/keel"
